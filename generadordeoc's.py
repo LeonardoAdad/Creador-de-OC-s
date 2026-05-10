eb2270025bb47dc46d272290b2dc69d918135705
RyotaGO <diegotellesc@gmail.com>
2026-05-10 11:26:35 -0600

    Ya nomas termina el resto de directorios

---
+++ generadordeoc's.py
@@ -0,0 +1,14 @@
+import random
+import time 
+
+Tematica = {
+    "Fantacia": {
+        "Profeciones": ["Mago", "Caballero", "Hada", "Arquero", "Asesino", "Realeza", "Plebeyo", "Bardo", "Bufon", "Nigromante"],
+        "Razas": ["Humano", "Elfo", "Enano", "Orco", "Goblin", "Tifling", "Celestial", "Draconido", "Mediano", "Semielfo"],
+        "Escenarios": ["Salon del trono", "Bosque magico", "escuela Mágica", "Montaña Misteriosa", "Isla Perdida", "Reino Subterráneo", "Caverna de Dragones", "Templo Antiguo", "villa", "Taberna"]
+    },
+    "Ciencia Ficcion": {
+        "Profeciones": ["Pandillero", "Hacker/Netrunner", "Corporativo", "Policia", "Mercenario", "Piloto", "Nomada", "Fixer", "Rockerboy", "Periodista"],
+        "Escenarios": ["Ciudad futurista", "Base espacial", "Barrios bajos", "Nave espacial", "Afueras de la ciudad", "Edificio corpotarivo", "Ciudad subterránea", "Bar lujoso", "Ciberespacio", "Bar clandestino"]
+    }
+    },
