1c5ee73ad5905d4508547ff4c3564a61d690b0d6
RyotaGO <diegotellesc@gmail.com>
2026-05-10 22:44:19 -0600

    Estructura de las ruletas

---
+++ Diccionarios.py
@@ -0,0 +1,63 @@
+import random
+import time 
+
+# ── Diccionarios ──
+escenarios = {
+    "Fantasía": [
+        "Salon del trono", "montaña misteriosa", "Bosque Mágico", "Aldea", "Templo antiguo",
+        "Taberna", "Caverna de dragones", "Isla Perdida", "Establo de caballos", "Escuela mágica"
+    ],
+    "Cyberpunk": [
+        "Bar clandestino", "Base espacial", "Ciudad futurista",
+        "Barrios Bajos", "Nave espacial", "Edificio corporativo",
+        "Bar lujoso", "Ciberespacio", "Ciudad subterránea", "Batalla de por territorio"
+    ],
+    "Actualidad": [
+        "El rancho", "Puestos callejeros", "Centro de la ciudad", "Cafetería",
+        "Oficina", "Barrio", "Calle Transitada", "Transporte público",
+        "Bar", "Parque"
+    ]
+}
+
+profesiones = {
+    "Fantasía": [
+        "Mago", "Caballero", "Nigromante", "Arquero", "Asesino",
+        "Realeza", "Plebeyo", "Bardo", "Arquero", "Bufon"
+    ],
+    "Cyberpunk": [
+        "Hacker/Netrunner", "Pandillero", "Nomada", "Fixer", "Corporativo",
+        "Policía", "Político", "Rockerboy", "Ingeniero", "Sicario"
+    ],
+    "Actualidad": [
+        "Doctor", "Estudiante", "Chofer", "Bartender", "Oficinista",
+        "Chef", "Artista", "Cholo", "Programador", "Abogado", "Profesor"
+    ]
+}
+
+Razas = {
+    "Humano", "Elfo", "Enano", "Orco", "Goblin", "Tifling", "Celestial", "Draconido", "Mediano", "Semielfo"
+}
+
+tematica = {
+    "Fantasía" , "Ciencia ficción" , "Actualidad"}
+
+genero = {
+    "Femenino" , "Masculino" , "No binario"}
+
+caracteristicas_fisicas = {
+    "Complexión": [
+        "Robusta", "Esbelta", "Media", "Definida"
+    ],
+    "Cabello": [
+        "Largo", "Medio", "Corto", "Pelón"
+    ],
+    "Color de piel": [
+        "Afro", "Moreno", "Blanco", "Olivo", "Albino", "Almendrado" "Rojo", "Verde", "Morado", "Azul", "Amarillo", "Rosa"
+    ]
+}
+
+colores_principales = {
+    "Fríos", "Calidos", "Pasteles "
+}
+
+
